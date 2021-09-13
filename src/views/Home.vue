78e11b70cfd9d9689bbcad9391ea0db739152638
Andrew Spearman <27811981+aspearman@users.noreply.github.com>
2021-09-13 08:55:54 +0000

    Display date, cases and deaths
    - Loading animation
    - Boxes components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,45 @@
 <template>
-  <div class="home">Hello World!</div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+  </main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data...</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="Loading..." />
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import DataTitle from "@/components/DataTitle.vue";
+import DataBoxes from "@/components/DataBoxes.vue";
 
 export default defineComponent({
   name: "Home",
-  components: {},
+  components: { DataTitle, DataBoxes },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
 });
 </script>
